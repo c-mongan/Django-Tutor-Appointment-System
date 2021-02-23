3b129f72538550bf5e6dc8f20277882b3f1b37d7
c-mongan <c17359016@mytudublin.ie>
2021-02-23 16:09:32 +0000

    Modified Register
    Redirect and flash message

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages #for flash messages
 
 
 def index(request):
@@ -10,5 +11,13 @@ def v1(response):
         return HttpResponse("View 1")
 
 def register(request):
-    form = UserCreationForm()
+    if request.method == "POST":
+        form = UserCreationForm(request.POST) #If it recieves a post request it means an account is being created
+        if form.is_valid():
+            username = form.cleaned_data.get('username') #If the username follows the condition it is allowed
+            messages.success(request,f'Account created for {username}!') #f string ---> this line is flash message
+            return redirect('index')
+
+    else:
+     form = UserCreationForm()
     return render(request,'main/register.html',{'form': form})
