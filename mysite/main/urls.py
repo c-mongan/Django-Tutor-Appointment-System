b34b598d133751c497339454caed04dc0eb93111
c-mongan <c17359016@mytudublin.ie>
2021-02-23 17:52:48 +0000

    Finished About & Home page
    Added html templates & CSS

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -5,6 +5,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
     path('v1/', views.v1, name='view 1'),
-      path('register/',views.register, name='register'),
+    path('register/',views.register, name='register'),
 ]
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -3,9 +3,32 @@ from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages #for flash messages
 
+posts = [
+    {  #bookings
 
+'author':'Conor', #Student name
+'title': 'Booking 1', #Booking ID
+'content':'Spanish', #Subject
+'date_posted': 'August 27th 2020'#Date booked
+    },
+{
+'author':'Andrew', #Student name
+'title': 'Booking 2', #Booking ID
+'content':'Math', #Subject
+'date_posted': 'December 17th 2020'#Date booked
+
+
+}
+]
 def index(request):
-    return HttpResponse("Tech with tim!")
+    context = {
+        'posts':posts #dictionary
+    }
+    return render (request,'main/home.html',context)
+
+def about(request):
+     return render (request,'main/about.html',{'title':'About'})
+
 
 def v1(response):
         return HttpResponse("View 1")
@@ -14,6 +37,7 @@ def register(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST) #If it recieves a post request it means an account is being created
         if form.is_valid():
+            form.save()
             username = form.cleaned_data.get('username') #If the username follows the condition it is allowed
             messages.success(request,f'Account created for {username}!') #f string ---> this line is flash message
             return redirect('index')
